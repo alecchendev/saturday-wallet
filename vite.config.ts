8902b3361671d783b0761b9cee1d47f03be7fe05
Alec Chen <alecchendev@gmail.com>
2023-07-29 14:51:47 -0500

    Add svg config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import solidSvg from 'vite-plugin-solid-svg';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     */
     // devtools(),
     solidPlugin(),
+    solidSvg(),
   ],
   server: {
     port: 3000,
